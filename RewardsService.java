72d7f454834df331a5bf69ae385cd32fdf0f900a
sai-sravani <83568366+sai-sravani@users.noreply.github.com>
2021-05-03 07:16:44 +0200

    Add files via upload

---
+++ RewardsService.java
@@ -0,0 +1,27 @@
+package com.mycompany.service.rewards.rewards;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.mycompany.service.rewards.model.Customer;
+
+@Service
+public class RewardsService {
+	
+	@Autowired
+	private CustomerRepository customerRepository;
+	
+	
+	public List<Customer> getCustomerAll() {
+		return customerRepository.findAll();
+	}
+	
+	public Customer getCustomerById(Integer customerId) {
+		return customerRepository.findById(customerId).orElse(null);
+	}
+
+}
